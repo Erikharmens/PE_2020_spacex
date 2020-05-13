b2d1120b8fa8a2be64311c35c91783904571f01c
Erikharmens <54328933+Erikharmens@users.noreply.github.com>
2020-05-13 17:28:35 +0200

    Fixed cards layout, removed old .js

--- Code/js/history.js
+++ Code/js/history.js
@@ -1,22 +1,29 @@
 const historyUrl = "https://api.spacexdata.com/v3/history";
 
 fetch(historyUrl)
-    .then(function(response){
+    .then(function (response) {
         return response.json();
         console.log(response);
     })
-    .then(function(json){
+    .then(function (json) {
         console.dir(json);
 
-        const historyElement = document.querySelector(".historyCards");
-        
+        const historyElement = document.querySelector(".historycontainer");
+
         let html = "";
 
-        for (let i = 0; i < json.length; i++){
+        for (let i = 0; i < json.length; i++) {
 
-            html += `<div class="historyCards">
-                        <p>${json[i].event_date_utc}</p>
-                        <h2>${json[i].title}</h2>
+            jsonLaunchDate = new Date(json[i].event_date_utc);
+
+            const isoDateFixed = "" + jsonLaunchDate.getDate() + "/" + (jsonLaunchDate.getMonth() + 1) + "/" + jsonLaunchDate.getFullYear();
+
+
+
+            html += `
+                    <div class="historycards">
+                    <h2>${json[i].title}</h2>
+                        <p>${isoDateFixed}</p>
                         <p>${json[i].details}</p>
                     </div>`
 
